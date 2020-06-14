69722f3bb5475ea0b4734cf8c0d06c804782b196
Andrey Molfly <andrey.molfly@gmail.com>
2020-06-14 20:29:56 +0500

    add day01/sentences.cbl

---
+++ day01/sentnces.cbl
@@ -0,0 +1,12 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. 
+       ENVIRONMENT DIVISION. 
+       DATA DIVISION. 
+       PROCEDURE DIVISION.
+
+       PROGRAM-BEGIN.
+           DISPLAY "This program contains four DIVISIONS,".
+           DISPLAY "three PARAGRAPHS".
+           DISPLAY "and four SENTENCES".
+       PROGRAM-DONE.
+           STOP RUN.
