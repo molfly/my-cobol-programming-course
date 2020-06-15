6a73f401dd96b3d4c3290cb6a670908cafeefc9e
Andrey Molfly <andrey.molfly@gmail.com>
2020-06-16 00:55:02 +0500

    fix 10 day02/hello02

---
+++ day02/hello02.cbl
@@ -0,0 +1,21 @@
+       IDENTIFICATION DIVISION. 
+       PROGRAM-ID. HELLO02.
+       ENVIRONMENT DIVISION. 
+       DATA DIVISION. 
+
+       WORKING-STORAGE SECTION. 
+
+       01  THE-NAME          PICTURE IS xxxxxxxxxx.
+
+       PROCEDURE DIVISION.
+       
+       PROGRAM-BEGIN.
+
+           DISPLAY "Enter someone's name.".
+
+           ACCEPT THE-NAME.
+
+           DISPLAY "Hello " THE-NAME.
+           
+       PROGRAM-DONE.
+           STOP RUN.
