33c8a5e58fc045ae7c441d83a0d2238c786b8439
Andrey Molfly <andrey.molfly@gmail.com>
2020-06-15 23:08:09 +0500

    add day02/helohelo

--- day02/helohelo.cbl
+++ day02/helohelo.cbl
@@ -1,11 +1,11 @@
        IDENTIFICATION DIVISION. 
-       PROGRAM-ID. HELLO.
+       PROGRAM-ID. HELOHELO.
        ENVIRONMENT DIVISION. 
        DATA DIVISION. 
        PROCEDURE DIVISION.
 
        PROGRAM-BEGIN.
+           DISPLAY "Hello world".
            DISPLAY "I said, Hello world".
-
        PROGRAM-DONE.
            STOP RUN.
