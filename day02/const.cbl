9e772041d3dcaaa7b6bfd23e85f56a0fcd3301a1
Andrey Molfly <andrey.molfly@gmail.com>
2020-06-15 23:14:54 +0500

    add day02/const

---
+++ day02/const.cbl
@@ -0,0 +1,12 @@
+       IDENTIFICATION DIVISION. 
+       PROGRAM-ID. CONST.
+       ENVIRONMENT DIVISION. 
+       DATA DIVISION. 
+       PROCEDURE DIVISION.
+
+       PROGRAM-BEGIN.
+           DISPLAY "Hello world".
+           DISPLAY 55.
+           DISPLAY 12.607.
+       PROGRAM-DONE.
+           STOP RUN.
