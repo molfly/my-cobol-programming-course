6b44a5331b8e43da584125b3b925e9f774da82db
Andrey Molfly <andrey.molfly@gmail.com>
2020-06-16 22:34:19 +0500

    add day03/add03

---
+++ day03/add03.cbl
@@ -0,0 +1,29 @@
+       IDENTIFICATION DIVISION. 
+       PROGRAM-ID. ADD03.
+       ENVIRONMENT DIVISION. 
+       DATA DIVISION.
+
+       WORKING-STORAGE SECTION.
+       
+       01  FIRST-NUMBER      PICTURE IS 99.
+       01  SECOND-NUMBER     PICTURE IS 99.
+       01  THE-RESULT        PICTURE IS 999.
+
+       PROCEDURE DIVISION.
+       
+       PROGRAM-BEGIN.
+           DISPLAY "This program will add 2 numbers.".
+           DISPLAY "Enter the first number.".
+
+           ACCEPT FIRST-NUMBER.
+
+           DISPLAY "Enter the second number.".
+
+           ACCEPT SECOND-NUMBER.
+
+           COMPUTE THE-RESULT = FIRST-NUMBER + SECOND-NUMBER.
+
+           DISPLAY "The result is " THE-RESULT.
+
+       PROGRAM-DONE.
+           STOP RUN.
