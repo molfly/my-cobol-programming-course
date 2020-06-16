ecc0d0bb8003facdd4dd745c58ffe2f67e87cf7c
Andrey Molfly <andrey.molfly@gmail.com>
2020-06-16 23:06:17 +0500

    add day03/hello04

---
+++ day03/hello04.cbl
@@ -0,0 +1,18 @@
+       IDENTIFICATION DIVISION. 
+       PROGRAM-ID. HELLO04.
+
+      *    This program illustrates the use of a PERFORM
+
+       ENVIRONMENT DIVISION. 
+       DATA DIVISION. 
+       PROCEDURE DIVISION.
+
+       PROGRAM-BEGIN.
+           DISPLAY "Today's massege is:".
+           PERFORM SAY-HELLO.
+
+       PROGRAM-DONE.
+           STOP RUN.
+
+       SAY-HELLO.
+           DISPLAY "Hello world".
